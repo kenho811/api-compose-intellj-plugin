e33307b991d28ad0afa29f0ab3c56fdccd86bcc4
GitHub Action <action@github.com>
2023-10-08 06:34:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/kenho811/apicomposeintelljplugin/MyBundle.kt
+++ src/main/kotlin/com/github/kenho811/apicomposeintelljplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.kenho811.apicomposeintelljplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/kenho811/apicomposeintelljplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kenho811/apicomposeintelljplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.kenho811.apicomposeintelljplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kenho811.apicomposeintelljplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
